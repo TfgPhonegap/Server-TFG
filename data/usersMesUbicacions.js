ffa90c6d9b4336818d8849c1a80ba3e0d5adc3b7
tfgphonegap <pol_8_v@hotmail.com>
2014-05-05 20:38:18 +0200

    NovaUbicacio modificada per acolpar-se a la nova estructura dubicacions

--- data/usersMesUbicacions.js
+++ data/usersMesUbicacions.js
@@ -2,17 +2,14 @@
 
     { name: 'Batman', description:'gotham lloc', avatar: 'img/avatars/batman.jpg', 
     ubicacions: [
-                {
-                  data: "01-01-2014",
-                  lloc: "Boston",
-                  comentari: "Comentari user"
-                },
-                {
-                  data: "24-11-1991",
-                  lloc: "Orlando",
-                  comentari: "Comentari user"
-                }
-              ],
+    { data: "01-01-2014", ubicacionsDia: [
+          {hora: "07:55", lloc: "Boston", comentari: "Comentari user"},
+          {hora: "23:59", lloc: "Orlando", comentari: "Comentari user"}
+              ]},
+    { data: "22-12-2013", ubicacionsDia: [
+          {hora: "07:55", lloc: "Boston", comentari: "Comentari user"},
+          {hora: "23:59", lloc: "Orlando", comentari: "Comentari user"}
+              ]}],
 
     accessos: [
     { data: '01-05-2001', accessosDia: [
@@ -27,18 +24,16 @@
         {hora: '16:00', lloc: 'Porta XXX', resultat: false}      
       ]
       }]},
-    { name: 'Spiderman', description:'web', avatar: 'img/avatars/spiderman.jpg' , ubicacions: [
-                {
-                  data: "01-01-2014",
-                  lloc: "Detroit",
-                  comentari: "Comentari user"
-                },
-                {
-                  data: "24-11-1991",
-                  lloc: "Chicago",
-                  comentari: "Comentari user"
-                }
-              ],
+    { name: 'Spiderman', description:'web', avatar: 'img/avatars/spiderman.jpg' , 
+    ubicacions: [
+    { data: "01-01-2014", ubicacionsDia: [
+          {hora: "07:55", lloc: "Boston", comentari: "Comentari user"},
+          {hora: "23:59", lloc: "Orlando", comentari: "Comentari user"}
+              ]},
+    { data: "22-12-2013", ubicacionsDia: [
+          {hora: "07:55", lloc: "Boston", comentari: "Comentari user"},
+          {hora: "23:59", lloc: "Orlando", comentari: "Comentari user"}
+              ]}],
        accessos: [
     { data: '01-05-2001', accessosDia: [
         {hora: '08:00', lloc: 'Entrada', resultat: true},
@@ -52,18 +47,16 @@
         {hora: '16:00', lloc: 'Porta XXX', resultat: false}      
       ]
       }]},
-    { name: 'Wolverine', description:'Not xman', avatar: 'img/avatars/wolverine.jpg' , ubicacions: [
-                {
-                  data: "01-01-2014",
-                  lloc: "OklahoComentari user",
-                  comentari: "Comentari user"
-                },
-                {
-                  data: "24-11-1991",
-                  lloc: "Boston",
-                  comentari: "Comentari user"
-                }
-              ],
+    { name: 'Wolverine', description:'Not xman', avatar: 'img/avatars/wolverine.jpg' , 
+             ubicacions: [
+    { data: "01-01-2014", ubicacionsDia: [
+          {hora: "07:55", lloc: "Boston", comentari: "Comentari user"},
+          {hora: "23:59", lloc: "Orlando", comentari: "Comentari user"}
+              ]},
+    { data: "22-12-2013", ubicacionsDia: [
+          {hora: "07:55", lloc: "Boston", comentari: "Comentari user"},
+          {hora: "23:59", lloc: "Orlando", comentari: "Comentari user"}
+              ]}],
            accessos: [
     { data: '01-05-2001', accessosDia: [
         {hora: '08:00', lloc: 'Entrada', resultat: true},
@@ -77,18 +70,16 @@
         {hora: '16:00', lloc: 'Porta XXX', resultat: false}      
       ]
       }]},
-    { name: 'Captain America', description:'USA', avatar: 'img/avatars/america.jpg' , ubicacions: [
-                {
-                  data: "01-01-2014",
-                  lloc: "Los Angeles",
-                  comentari: "Comentari user"
-                },
-                {
-                  data: "24-11-1991",
-                  lloc: "Boston",
-                  comentari: "Comentari user"
-                }
-              ],
+    { name: 'Captain America', description:'USA', avatar: 'img/avatars/america.jpg' ,
+               ubicacions: [
+    { data: "01-01-2014", ubicacionsDia: [
+          {hora: "07:55", lloc: "Boston", comentari: "Comentari user"},
+          {hora: "23:59", lloc: "Orlando", comentari: "Comentari user"}
+              ]},
+    { data: "22-12-2013", ubicacionsDia: [
+          {hora: "07:55", lloc: "Boston", comentari: "Comentari user"},
+          {hora: "23:59", lloc: "Orlando", comentari: "Comentari user"}
+              ]}],
 
                accessos: [
     { data: '01-05-2001', accessosDia: [
@@ -103,18 +94,16 @@
         {hora: '16:00', lloc: 'Porta XXX', resultat: false}      
       ]
       }]},
-    { name: 'Ironman', description:'Money', avatar: 'img/avatars/ironman.png' , ubicacions: [
-                {
-                  data: "01-01-2014",
-                  lloc: "San Diego",
-                  comentari: "Comentari user"
-                },
-                {
-                  data: "24-11-1991",
-                  lloc: "Boston",
-                  comentari: "Comentari user"
-                }
-              ],
+    { name: 'Ironman', description:'Money', avatar: 'img/avatars/ironman.png' ,
+             ubicacions: [
+    { data: "01-01-2014", ubicacionsDia: [
+          {hora: "07:55", lloc: "Boston", comentari: "Comentari user"},
+          {hora: "23:59", lloc: "Orlando", comentari: "Comentari user"}
+              ]},
+    { data: "22-12-2013", ubicacionsDia: [
+          {hora: "07:55", lloc: "Boston", comentari: "Comentari user"},
+          {hora: "23:59", lloc: "Orlando", comentari: "Comentari user"}
+              ]}],
 
                accessos: [
     { data: '01-05-2001', accessosDia: [
@@ -129,18 +118,16 @@
         {hora: '16:00', lloc: 'Porta XXX', resultat: false}      
       ]
       }]},
-    { name: 'Fantastic four', description:'Lets go', avatar: 'img/avatars/4.jpg' , ubicacions: [
-                {
-                  data: "01-01-2014",
-                  lloc: "Detroit",
-                  comentari: "Comentari user"
-                },
-                {
-                  data: "24-11-1991",
-                  lloc: "Boston",
-                  comentari: "Comentari user"
-                }
-              ],
+    { name: 'Fantastic four', description:'Lets go', avatar: 'img/avatars/4.jpg' , 
+               ubicacions: [
+    { data: "01-01-2014", ubicacionsDia: [
+          {hora: "07:55", lloc: "Boston", comentari: "Comentari user"},
+          {hora: "23:59", lloc: "Orlando", comentari: "Comentari user"}
+              ]},
+    { data: "22-12-2013", ubicacionsDia: [
+          {hora: "07:55", lloc: "Boston", comentari: "Comentari user"},
+          {hora: "23:59", lloc: "Orlando", comentari: "Comentari user"}
+              ]}],
 
                accessos: [
     { data: '01-05-2001', accessosDia: [
@@ -155,18 +142,16 @@
         {hora: '16:00', lloc: 'Porta XXX', resultat: false}      
       ]
       }]},
-    { name: 'Thor', description:'!!!', avatar: 'img/avatars/thor.jpg' , ubicacions: [
-                {
-                  data: "01-01-2014",
-                  lloc: "Chicago",
-                  comentari: "Comentari user"
-                },
-                {
-                  data: "24-11-1991",
-                  lloc: "Boston",
-                  comentari: "Comentari user"
-                }
-              ],
+    { name: 'Thor', description:'!!!', avatar: 'img/avatars/thor.jpg' , 
+             ubicacions: [
+    { data: "01-01-2014", ubicacionsDia: [
+          {hora: "07:55", lloc: "Boston", comentari: "Comentari user"},
+          {hora: "23:59", lloc: "Orlando", comentari: "Comentari user"}
+              ]},
+    { data: "22-12-2013", ubicacionsDia: [
+          {hora: "07:55", lloc: "Boston", comentari: "Comentari user"},
+          {hora: "23:59", lloc: "Orlando", comentari: "Comentari user"}
+              ]}],
 
               accessos: [
     { data: '01-05-2001', accessosDia: [
@@ -181,18 +166,16 @@
         {hora: '16:00', lloc: 'Porta XXX', resultat: false}      
       ]
       }]},
-    { name: 'Hulk', description:'Bussy', avatar: 'img/avatars/hulk.jpg', ubicacions: [
-                {
-                  data: "01-01-2014",
-                  lloc: "New York",
-                  comentari: "Comentari user"
-                },
-                {
-                  data: "24-11-1991",
-                  lloc: "Boston",
-                  comentari: "Comentari user"
-                }
-              ],
+    { name: 'Hulk', description:'Bussy', avatar: 'img/avatars/hulk.jpg', 
+              ubicacions: [
+    { data: "01-01-2014", ubicacionsDia: [
+          {hora: "07:55", lloc: "Boston", comentari: "Comentari user"},
+          {hora: "23:59", lloc: "Orlando", comentari: "Comentari user"}
+              ]},
+    { data: "22-12-2013", ubicacionsDia: [
+          {hora: "07:55", lloc: "Boston", comentari: "Comentari user"},
+          {hora: "23:59", lloc: "Orlando", comentari: "Comentari user"}
+              ]}],
                accessos: [
     { data: '01-05-2001', accessosDia: [
         {hora: '08:00', lloc: 'Entrada', resultat: true},
--- handlers/ubicacions.js
+++ handlers/ubicacions.js
@@ -35,6 +35,7 @@ exports.novaUbicacio = function(req, res){
 	var nouLloc = req.param("lloc");
 	var nouComentari = req.param("comentari");
 	var novaData = req.param("data");
+	var novaHora = req.param("hora");
 	var usuari = req.params.userName;
 
 	User.findOne({name: usuari}, function(err, doc){
@@ -46,7 +47,59 @@ exports.novaUbicacio = function(req, res){
 				res.send(req.params.userName + " no és cap user");
 			}
 			else {
+				//Busquem si ja existeix alguna ubicació amb aquella data:
+				var dataTrobada=false;
+				var i=0;
+				for (i; i<doc.ubicacions.length; i++) {
+					if (novaData == doc.ubicacions[i].data) {
+						console.log('Aquesta data ' + doc.ubicacions[i].data + ' és la que buscava' );
+						dataTrobada=true;
+						break;
+					}
+				}
+				
 				var query = {name: usuari};
+				var update = {ubicacions: doc.ubicacions};
+				var options = {new: true};
+				if (dataTrobada) {
+					doc.ubicacions[i].ubicacionsDia.unshift({comentari: nouComentari,
+			                  lloc: nouLloc,
+			                  hora: novaHora});
+					User.findOneAndUpdate(query, update, options, function(err, user) {
+					  if (err) {
+					    console.log('got an error');
+					    console.log(err);
+					    res.send(false);
+					  }
+					  else {
+					  	console.log('No hi ha hagut error :)))))');
+					  	console.log(user);
+					  	resultat = user;
+					  	res.send(doc.ubicacions);
+					  }
+					});
+				}
+				else {
+					doc.ubicacions.unshift({data: novaData, ubicacionsDia: [{comentari: nouComentari,
+			                  lloc: nouLloc,
+			                  hora: novaHora}]});					
+					User.findOneAndUpdate(query, update, options, function(err, user) {
+					  if (err) {
+					    console.log('got an error');
+					    console.log(err);
+					    res.send(false);
+					  }
+					  else {
+					  	console.log('No hi ha hagut error :)))))');
+					  	console.log(user);
+					  	resultat = user;
+					  	res.send(doc.ubicacions);
+					  }
+					});
+				}
+
+
+				/*var query = {name: usuari};
 				// Afegim la nova ubicació a l'inici de la llista
 				doc.ubicacions.unshift({comentari: nouComentari,
 			                  lloc: nouLloc,
@@ -66,7 +119,7 @@ exports.novaUbicacio = function(req, res){
 				  	resultat = user;
 				  	res.send(true);
 				  }
-				});
+				});*/
 			}
 		}
 		
